0aee308446389f105babe021efd9048487414d18
reemkhd <reemnote@gmail.com>
2021-01-31 07:31:16 +0300

    update code

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffeeshop-reem.us', // the auth0 domain prefix
+    audience: 'reem', // the audience set for the auth0 app
+    clientId: 'HyWZXGq28c4YIKTDL26Nl1Vm5wByeG2t', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
